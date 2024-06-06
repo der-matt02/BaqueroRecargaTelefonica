e7680e3deb1787f26e5fb1a6ca9b2ccb6912c74a
der-matt02 <dermatt02@gmail.com>
2024-06-06 08:06:43 -0500

    Parte visual creada y funcional

---
+++ Models/Recarga.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaqueroRecargaTelefonica.Models
+{
+    public class Recarga
+    {
+        public string NumeroTelefono { get; set; }
+        public string Operador { get; set; }
+        public decimal Monto { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+namespace BaqueroRecargaTelefonica.Views;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        radio3.CheckedChanged += MA_OnMontoChanged;
+        radio5.CheckedChanged += MA_OnMontoChanged;
+        radio10.CheckedChanged += MA_OnMontoChanged;
+    }
+
+    private void MA_OnMontoChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (e.Value)
+        {
+            var radioButton = (RadioButton)sender;
+            DisplayAlert("Monto Seleccionado", $"Has seleccionado {radioButton.Content}", "OK");
+        }
+    }
+
+    private async void MA_btnRecargar_Clicked(object sender, EventArgs e)
+    {
+        string telefono = inputTelefono.Text;
+        string operador = pickerOperador.SelectedItem?.ToString();
+        string monto = null;
+
+        if (radio3.IsChecked)
+            monto = "3 dólares";
+        else if (radio5.IsChecked)
+            monto = "5 dólares";
+        else if (radio10.IsChecked)
+            monto = "10 dólares";
+
+        if (string.IsNullOrEmpty(telefono) || string.IsNullOrEmpty(operador) || string.IsNullOrEmpty(monto))
+        {
+            await DisplayAlert("Error", "Por favor, complete todos los campos.", "OK");
+            return;
+        }
+
+        
+        await MA_RealizarRecargaAsync(telefono, operador, monto);
+    }
+
+    private async Task MA_RealizarRecargaAsync(string telefono, string operador, string monto)
+    {
+        
+        await Task.Delay(2000);
+
+        string mensaje = $"Se hizo una recarga de {monto} en la siguiente fecha: {DateTime.Now:dd/MM/yyyy}";
+        string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), $"{telefono}.txt");
+
+        
+        File.WriteAllText(filePath, mensaje);
+
+        
+        lblMensaje.Text = "Recarga realizada con éxito";
+        lblMensaje.IsVisible = true;
+
+        await DisplayAlert("Confirmación", "Su recarga se ha realizado con éxito.", "OK");
+    }
+}
